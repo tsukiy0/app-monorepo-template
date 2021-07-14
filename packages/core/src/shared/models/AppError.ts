6c05e68d32d62e19adb15fa30c856e2d510d1e05
tsukiy0 <tsukiy0@users.noreply.github.com>
2021-07-14 23:01:27 +1000

    refactor: remove unused (available from tsukiy0/extensions-core)

--- packages/core/src/shared/models/AppError.ts
+++
@@ -1,7 +0,0 @@
-export abstract class AppError extends Error {
-  constructor(message?: string) {
-    super(message);
-    this.name = this.constructor.name;
-    Object.setPrototypeOf(this, new.target.prototype);
-  }
-}
--- packages/core/src/shared/services/ILogger.ts
+++
@@ -1,4 +0,0 @@
-export interface ILogger {
-  info(message: string, context?: unknown): void;
-  error(e: Error, context?: unknown): void;
-}
