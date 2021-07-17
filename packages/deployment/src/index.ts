68f36960198926835b6aaad96bfa46fc05207d96
tsukiy0 <tsukiy0@users.noreply.github.com>
2021-07-17 11:42:27 +1000

    refactor: use IConfiguration to get deployment config

--- packages/deployment/src/index.ts
+++ packages/deployment/src/index.ts
@@ -1,20 +1,25 @@
+import { SystemConfiguration } from "@tsukiy0/extensions-core";
 import { App } from "aws-cdk-lib";
 import { AppStack } from "./stacks/AppStack";
 import { ExternalStack } from "./stacks/ExternalStack";
 
 const app = new App();
+const config = new SystemConfiguration();
+
+const account = config.get("CDK_DEFAULT_ACCOUNT");
+const alertEmail = config.get("ALERT_EMAIL");
 
 new ExternalStack(app, "External", {
   env: {
-    account: process.env.CDK_DEFAULT_ACCOUNT!,
+    account,
     region: "us-east-1",
   },
-  alertEmail: process.env.ALERT_EMAIL!,
+  alertEmail,
 });
 
 new AppStack(app, "AppUsEast1", {
   env: {
-    account: process.env.CDK_DEFAULT_ACCOUNT!,
+    account,
     region: "us-east-1",
   },
 });
