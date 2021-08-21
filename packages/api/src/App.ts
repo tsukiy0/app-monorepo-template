64f83496b967ce5024b7ee227819ec09b3e34fdb
tsukiy0 <tsukiy0@users.noreply.github.com>
2021-08-21 16:44:24 +1000

    refactor: move health endpoint into PublicRouter

--- packages/api/src/App.ts
+++ packages/api/src/App.ts
@@ -1,10 +1,8 @@
 import express, { Application } from "express";
-import { SystemConfiguration } from "@tsukiy0/extensions-core";
 import {
   CorrelationMiddleware,
   ErrorMiddleware,
   LoggerMiddleware,
-  promisifyHandler,
 } from "@tsukiy0/extensions-express";
 import { PublicRouter } from "./routers/PublicRouter";
 import { ServicesMiddleware } from "./middlewares/ServicesMiddleware";
@@ -13,8 +11,6 @@ export class App {
   static build = (): Application => {
     const app = express();
 
-    const configuration = new SystemConfiguration();
-
     const correlationMiddleware = new CorrelationMiddleware();
     const loggerMiddleware = new LoggerMiddleware(
       "@app/api",
@@ -22,20 +18,13 @@ export class App {
     );
     const servicesMiddleware = new ServicesMiddleware(correlationMiddleware);
     const errorMiddleware = new ErrorMiddleware(loggerMiddleware);
-    const publicRouter = new PublicRouter(configuration);
+    const publicRouter = new PublicRouter();
 
     app.use(correlationMiddleware.handler);
     app.use(loggerMiddleware.handler);
     app.use(servicesMiddleware.handler);
 
     app.use(publicRouter.router);
-
-    app.get(
-      "/health",
-      promisifyHandler(async (req, res) => {
-        res.status(200).end();
-      }),
-    );
 
     app.use(errorMiddleware.handler);
 
--- packages/api/src/routers/PublicRouter.ts
+++ packages/api/src/routers/PublicRouter.ts
@@ -1,24 +1,14 @@
-import cors from "cors";
-import { json } from "body-parser";
 import { Router } from "express";
-import { IConfiguration } from "@tsukiy0/extensions-core";
 import { promisifyHandler } from "@tsukiy0/extensions-express";
 
 export class PublicRouter {
-  constructor(private readonly config: IConfiguration) {}
-
   router = (): Router => {
     const router = Router();
 
-    router.use(cors());
-    router.use(json());
-
-    router.post(
-      "/config",
+    router.get(
+      "/v1/public/health",
       promisifyHandler(async (req, res) => {
-        res.status(200).json({
-          config: this.config.get("TEST"),
-        });
+        res.status(200).end();
       }),
     );
 
