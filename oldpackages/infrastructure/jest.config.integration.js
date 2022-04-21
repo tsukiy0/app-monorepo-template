2b8117e2c9ad545dfebf47177a94555f475b2aeb
tsukiy0 <tsukiy0@users.noreply.github.com>
2022-04-21 12:22:56 +0000

    feat: build api with webpack

--- oldpackages/infrastructure/jest.config.integration.js
+++
@@ -1,8 +0,0 @@
-const base = require("./jest.config");
-
-module.exports = {
-  ...base,
-  testMatch: ["**/*.integrationTest.ts"],
-  testTimeout: 60000,
-  setupFiles: ["./setupIntegrationTests.js"],
-};
